8626382d2f63047ad707835a69e00dc99c246c44
Marcos Antonio Cordovil de Oliveira <marcoantonio.caco@gmail.com>
2018-12-29 16:00:26 -0200

    COLAB PATH UPDATES

--- tf_pose/pafprocess/setup.py
+++ tf_pose/pafprocess/setup.py
@@ -5,7 +5,7 @@ import os
 # os.environ['CC'] = 'g++';
 setup(name='pafprocess_ext', version='1.0',
     ext_modules=[
-        Extension('_pafprocess', ['pafprocess.cpp', 'pafprocess.i'],
+        Extension('_pafprocess', ['/content/drive/My Drive/Colab Notebooks/BioMec/tf_pose/pafprocess/pafprocess.cpp', '/content/drive/My Drive/Colab Notebooks/BioMec/tf_pose/pafprocess/pafprocess.i'],
                   swig_opts=['-c++'],
                   depends=["pafprocess.h"],
                   include_dirs=[numpy.get_include(), '/content/drive/My Drive/Colab Notebooks/BioMec/tf_pose/pafprocess/'])
